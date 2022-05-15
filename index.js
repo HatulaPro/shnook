2051a4d77eee81557ecf1ac6062f29af33a434f7
shalev280804@gmail.com <shalev280804@gmail.com>
2022-05-15 06:10:28 -0400

    init firebase

--- index.js
+++ index.js
@@ -7,9 +7,24 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
-app.use('/', express.static('public'));
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+app.use('/public', express.static('public'));
 app.get('/', (req, res) => {
-	res.sendFile('public/index.html');
+	res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/create', (req, res) => {
+	res.sendFile(__dirname + '/views/create.html');
+});
+
+app.get('/join', (req, res) => {
+	res.sendFile(__dirname + '/views/join.html');
 });
 
 io.on('connection', (socket) => {
