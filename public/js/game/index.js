ffcffe16ee3142eb46e673346c7b790889e55312
shalev280804@gmail.com <shalev280804@gmail.com>
2022-06-06 13:49:00 -0400

    Username input is now focused automatically

--- public/js/game/index.js
+++ public/js/game/index.js
@@ -453,6 +453,8 @@ function setState(s) {
 		timerSpan.style.display = 'none';
 		challengeDiv.style.display = 'none';
 
+		usernameInput.focus();
+
 		roomIdTitle.innerText = '';
 		roomPlayers.innerText = '';
 		roomRounds.innerText = '';
