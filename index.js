54a095d1aa22726dd786c1f372b53c14534387ce
shalev280804@gmail.com <shalev280804@gmail.com>
2022-06-10 13:17:10 -0400

    Removed firebase files

--- index.js
+++ index.js
@@ -4,15 +4,6 @@ const PORT = 3000;
 const http = require('http');
 const server = http.createServer(app);
 
-const admin = require('firebase-admin');
-
-const serviceAccount = require('./serviceAccount.json');
-
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: 'https://shnook-ccd58-default-rtdb.europe-west1.firebasedatabase.app/',
-});
-
 const { Server } = require('socket.io');
 const io = new Server(server);
 
